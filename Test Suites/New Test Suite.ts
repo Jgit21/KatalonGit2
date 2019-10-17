1e9f0d9066d3572739b904750d21c2cf3c3bcca9
j <j1@gmail.com>
2019-10-17 10:30:46 -0300

    TC Login - Variables Excel

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>65c2b720-6cfa-4b20-a5e1-297fa08c3b27</testSuiteGuid>
+   <testCaseLink>
+      <guid>40294a7a-2430-4314-b2b5-7bc177c6458c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC Login - Variables TC</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>73fbd39b-10f3-4501-9650-7d1b19fe722c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a518ff36-219e-4f0d-b358-1fefac8f477c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>45143c16-99f1-4733-9cee-9f813c3abe1a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
