a7172d4270e16bf2b4a6ebf5ef552e98d8b76779
j <j1@gmail.com>
2019-10-17 11:12:59 -0300

    TS - Var toman data desde excel

---
+++ Test Suites/TS - Var toman data desde excel.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS - Var toman data desde excel</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8b7302b1-ce14-43a8-99d2-7d8353d4f58b</testSuiteGuid>
+   <testCaseLink>
+      <guid>cf474b82-1592-407a-9e30-20a3b97a0064</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC Login - Variables TC</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>d8347fb9-4eb3-4646-974b-e1290341d7b4</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>2-3</value>
+         </iterationEntity>
+         <testDataId>Data Files/TestDataExcelPrueba1</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>73fbd39b-10f3-4501-9650-7d1b19fe722c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>d8347fb9-4eb3-4646-974b-e1290341d7b4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>usuario</value>
+         <variableId>a518ff36-219e-4f0d-b358-1fefac8f477c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>d8347fb9-4eb3-4646-974b-e1290341d7b4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>clave</value>
+         <variableId>45143c16-99f1-4733-9cee-9f813c3abe1a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
